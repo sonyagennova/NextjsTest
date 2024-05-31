287752f6ceec365dbbe9cca2299db5492b2f4259
Sonya Genova <sonyagennova@gmail.com>
2024-05-31 17:35:58 +0300

    Edited description (First commit)

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { Hero } from "../sections";
 const heroProps = {
   image: { src: "/img/heroimg.png", alt: "", width: 1000, height: 1680 },
   title: "Agency procurement, outsourced.",
-  description: "Start the process here",
+  description: "Start the process here.",
   ctaText: "Start",
 };
 
