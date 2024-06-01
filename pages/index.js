2949e2b475cb540e81d69b9bfcbce47665f180b2
Sonya Genova <sonyagennova@gmail.com>
2024-06-01 17:54:36 +0300

    Changed the title and description

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import { Hero } from "../sections";
 
 const heroProps = {
-  image: { src: "/img/heroimg.png", alt: "", width: 1000, height: 1680 },
-  title: "Agency procurement, outsourced.",
-  description: "Start the process here.",
+  image: { src: "/img/video.png", alt: "", width: 1000, height: 1680 },
+  title: "Managed agency selection",
+  description: "Strengthen your onboarding process",
   ctaText: "Start",
 };
 
@@ -17,6 +17,16 @@ export default function Home() {
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
         />
+        <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+    />
+        <style>{`
+        .body-container {
+          background-image: url('/img/background.png');
+        }
+        `}
+        </style>
       </head>
       <div>
         <Hero {...heroProps} />
