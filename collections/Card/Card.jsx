3753d744ee3c010d346c7acab83aeb4945971781
Sonya Genova <sonyagennova@gmail.com>
2024-06-01 17:55:14 +0300

    Created a Card collection

--- collections/Card/Card.jsx
+++ collections/Card/Card.jsx
@@ -1,1 +1,20 @@
+import { BriefComponent } from "../../components/Brief"
+import { StyledBriefComponent } from "../../components/Brief/elements"
+import { StyledCardContainer } from "../../components/Containers/SectionContainer/elements"
+import { PitchComponent } from "../../components/Pitch"
+import { StyledPitchComponent } from "../../components/Pitch/elements"
+import { SearchComponent } from "../../components/Search"
+import { StyledSearchComponent } from "../../components/Search/elements"
+
 // The Card to be exported goes here
+export const Card = () => {
+    return (
+        <>
+            <StyledCardContainer>
+                <StyledBriefComponent><BriefComponent/></StyledBriefComponent>
+                <StyledSearchComponent><SearchComponent/></StyledSearchComponent>
+                <StyledPitchComponent><PitchComponent/></StyledPitchComponent>
+            </StyledCardContainer>
+        </>
+    )
+}
--- collections/Card/elements.jsx
+++ collections/Card/elements.jsx
@@ -1,1 +1,23 @@
-// Styled elements for the Card go here
+import styled from "styled-components";
+
+export const StyledCardInnerText = styled((props) => <div {...props}/>)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const StyledCardInnerContainer = styled((props) => <div {...props}/>)`
+  display: flex;
+  height: 110px;
+  border-radius: 6px;
+  padding: 0 20px;
+  gap: 20px;
+  border: 2px white solid;
+`;
+
+export const StyledCardInnerImage = styled((props) => <div {...props}/>)`
+  display: flex;
+  width: 100px;
+  align-items: center;
+  justify-content: center;
+`;
