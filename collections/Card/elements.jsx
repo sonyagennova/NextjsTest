d1e0165695b01e7fe7c0c8b089b113825c3f0576
Sonya Genova <sonyagennova@gmail.com>
2024-06-02 15:08:58 +0300

    Updated cards sizes

--- collections/Card/elements.jsx
+++ collections/Card/elements.jsx
@@ -1,17 +1,18 @@
 import styled from "styled-components";
 
 export const StyledCardInnerText = styled((props) => <div {...props}/>)`
-  display: flex;
   flex-direction: column;
   justify-content: center;
+  display: flex;
+  
 `;
 
 export const StyledCardInnerContainer = styled((props) => <div {...props}/>)`
   display: flex;
   height: 110px;
   border-radius: 6px;
-  padding: 0 20px;
-  gap: 20px;
+  padding: 20px 40px;
+  gap: 40px;
   border: 2px white solid;
 `;
 
