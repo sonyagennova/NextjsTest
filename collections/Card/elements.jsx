5a19eef6f96a7a8adfefe96352e93cbd8c6eb534
Sonya Genova <sonyagennova@gmail.com>
2024-06-02 15:38:08 +0300

    Added hover functions

--- collections/Card/elements.jsx
+++ collections/Card/elements.jsx
@@ -4,7 +4,6 @@ export const StyledCardInnerText = styled((props) => <div {...props}/>)`
   flex-direction: column;
   justify-content: center;
   display: flex;
-  
 `;
 
 export const StyledCardInnerContainer = styled((props) => <div {...props}/>)`
@@ -14,6 +13,13 @@ export const StyledCardInnerContainer = styled((props) => <div {...props}/>)`
   padding: 20px 40px;
   gap: 40px;
   border: 2px white solid;
+  :hover p{
+    color: #3a6ef7;
+    text-decoration: underline;
+  }
+  :hover{
+    border: 2px #3a6ef7 solid;
+  }
 `;
 
 export const StyledCardInnerImage = styled((props) => <div {...props}/>)`
